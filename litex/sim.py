6f48716614acda17d697ebc489f0bcbfd028e7ae
Marek Materzok <tilk@tilk.eu>
2022-01-19 13:48:37 +0100

    Towards soft JTAG

--- litex/sim.py
+++ litex/sim.py
@@ -149,10 +149,7 @@ class JTAGTAPFSM(Module):
             }
         if expose_signals:
             for state_name in fsm.actions:
-                reset_val = 0
-                if state_name == 'test_logic_reset':
-                    reset_val = 1
-                sig = fsm.ongoing(state_name, reset=reset_val)
+                sig = fsm.ongoing(state_name)
                 SHOUTING_NAME = state_name.upper()
                 hcs_name = SHOUTING_NAME
                 hcs = Signal(name=hcs_name)
@@ -162,13 +159,43 @@ class JTAGTAPFSM(Module):
 # Softcore TAP -------------------------------------------------------------------------------------
 # TODO: merge to LiteX
 
+class JTAGPort(Module):
+    def __init__(self, insn, with_enable=False):
+        self.insn = insn
+
+        self.tck = Signal()
+        self.tdi = Signal()
+        self.tdo = Signal()
+
+        self.reset = Signal()
+        self.capture = Signal()
+        self.shift = Signal()
+        self.update = Signal()
+        if with_enable:
+            self.enable = Signal()
+
 class SoftJTAG(Module):
-    def __init__(self, pads):
-        self.tck = tck = pads.tck
-        self.tms = tms = pads.tms
-        self.tdi = tdi = pads.tdi
+    def __init__(self, insn_width, pads=None):
+        self.insn_width = insn_width
+        self.ports = []
+
+        # JTAG signals
+        self.trst = trst = Signal()
+        self.tck = tck = Signal()
+        self.tms = tms = Signal()
+        self.tdi = tdi = Signal()
         self.tdo = tdo = Signal()
 
+        # JTAG clock domains
+        self.clock_domains.cd_jtag  = cd_jtag  = ClockDomain()
+        self.clock_domains.cd_jtagn = cd_jtagn = ClockDomain()
+
+        self.comb += cd_jtag.clk.eq(tck)
+        self.comb += cd_jtag.rst.eq(trst)
+        self.comb += cd_jtagn.clk.eq(~tck)
+        self.comb += cd_jtagn.rst.eq(trst)
+
+        # JTAG TAP state machine
         self.submodules.tap_fsm = tap_fsm = JTAGTAPFSM(tms, tck)
 
         self.reset = tap_fsm.TEST_LOGIC_RESET
@@ -176,7 +203,64 @@ class SoftJTAG(Module):
         self.shift = tap_fsm.SHIFT_DR
         self.update = tap_fsm.UPDATE_DR
 
-        self.comb += pads.tdo.eq(tdo)
+        # JTAG TAP logic
+        insnShift = Signal(insn_width)
+        self.insn = insn = Signal(insn_width)
+        self.bypass = bypass = Signal()
+        self.tdoComb = tdoComb = Signal()
+        self.sync.jtag += If(tap_fsm.TEST_LOGIC_RESET,
+                insnShift.eq(0)
+            ).Elif(tap_fsm.CAPTURE_IR,
+                insnShift.eq(5) # ???
+            ).Elif(tap_fsm.SHIFT_IR,
+                insnShift.eq(Cat(insnShift[1:], tdi))
+            )
+        self.sync.jtagn += If(tap_fsm.TEST_LOGIC_RESET,
+                insn.eq(0)
+            ).Elif(tap_fsm.UPDATE_IR,
+                insn.eq(insnShift)
+            )
+        self.sync.jtag += bypass.eq(tdi)
+        self.sync.jtagn += tdo.eq(tdoComb)
+
+        if pads:
+            self.connect(pads)
+
+    def connect(self, pads):
+        if hasattr(pads, 'trst'):
+            self.comb += self.trst.eq(pads.trst)
+        else:
+            self.comb += self.trst.eq(0)
+        self.comb += self.tck.eq(pads.tck)
+        self.comb += self.tms.eq(pads.tms)
+        self.comb += self.tdi.eq(pads.tdi)
+        self.comb += pads.tdo.eq(self.tdo)
+
+    def add_port(self, port):
+        self.ports.append(port)
+
+    def do_finalize(self):
+
+        insncase = {}
+        insncase[C(-1, (self.insn_width, False))] = self.tdo.eq(self.bypass)
+        for port in self.ports:
+            self.comb += port.tdi.eq(self.tdi)
+            self.comb += port.tck.eq(self.tck)
+            enable = self.insn == port.insn
+            if hasattr(port, 'enable'):
+                self.comb += port.enable.eq(enable)
+                self.comb += port.reset.eq(self.reset)
+                self.comb += port.capture.eq(self.capture)
+                self.comb += port.shift.eq(self.shift)
+                self.comb += port.update.eq(self.update)
+            else:
+                self.comb += port.reset.eq(self.reset & enable)
+                self.comb += port.capture.eq(self.capture & enable)
+                self.comb += port.shift.eq(self.shift & enable)
+                self.comb += port.update.eq(self.update & enable)
+            insncase[C(port.insn, (self.insn_width, False))] = self.tdo.eq(port.tdo)
+
+        self.comb += Case(self.insn, insncase)
 
 # SimSoC -------------------------------------------------------------------------------------------
 
@@ -240,7 +324,20 @@ class SimSoC(SoCCore):
         
         # JTAG -------------------------------------------------------------------------------------
         if jtag:
-            pass
+            softJtag = SoftJTAG(4)
+            vexPort = JTAGPort(0, True)
+            softJtag.add_port(vexPort)
+            softJtag.connect(platform.request("jtag"))
+            self.comb += self.cpu.jtag_clk.eq(vexPort.tck)
+            self.comb += self.cpu.jtag_tdi.eq(vexPort.tdi)
+            self.comb += vexPort.tdo.eq(self.cpu.jtag_clk)
+            self.comb += self.cpu.jtag_enable.eq(vexPort.enable)
+            self.comb += self.cpu.jtag_capture.eq(vexPort.capture)
+            self.comb += self.cpu.jtag_shift.eq(vexPort.shift)
+            self.comb += self.cpu.jtag_update.eq(vexPort.update)
+            self.comb += self.cpu.jtag_reset.eq(vexPort.reset)
+            self.submodules += softJtag
+            self.submodules += vexPort
 
     def do_finalize(self):
         self.add_constant("ROM_BOOT_ADDRESS", self.bus.regions["opensbi"].origin)
@@ -255,7 +352,7 @@ def main():
     parser.add_argument("--trace-end",            default=-1,               help="cycle to end VCD tracing")
     parser.add_argument("--opt-level",            default="O3",             help="compilation optimization level")
     parser.add_argument("--threads",              default=1,                help="set number of threads (default=1)")
-    parser.add_argument("--jtagremote",           default=False, type=bool, help="enable debugging via jtagremote")
+    parser.add_argument("--jtagremote",           action="store_true",      help="enable debugging via jtagremote")
     VexRiscvSMP.args_fill(parser)
     args = parser.parse_args()
 
@@ -263,7 +360,7 @@ def main():
     sim_config = SimConfig(default_clk="sys_clk")
     sim_config.add_module("serial2console", "serial")
     if args.jtagremote:
-        sim_config.add_module("jtagremote", "jtag")
+        sim_config.add_module("jtagremote", "jtag", args={"port": 1234})
     board_name = "sim"
     build_dir  = os.path.join("build", board_name)
 
